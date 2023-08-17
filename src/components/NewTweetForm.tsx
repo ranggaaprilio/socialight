5f7ab5daebc0590a56efc1cfbed7daf4538e7028
Rangga Aprilio Utama <ranggaaprillio@gmail.com>
2023-08-17 09:11:20 +0700

    update text Area

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,11 +1,39 @@
+import { useSession } from "next-auth/react";
 import { Button } from "./Button";
+import { ProfileImage } from "./ProfileImage";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+function updateTextAreaSize(textArea: HTMLTextAreaElement) {
+  if (textArea == null) return;
+  textArea.style.height = "0px";
+  textArea.style.height = `${textArea.scrollHeight}px`;
+}
 
 export function NewTweetForm() {
+  const session = useSession();
+  const [inputValue, setInputValue] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+
+  useEffect(() => {
+    updateTextAreaSize(textAreaRef.current!);
+  }, [inputValue]);
+
+  if (session.status !== "authenticated") return null;
+
   return (
     <form className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
-        {/* <ProfileImage src="#url"/> */}
+        <ProfileImage src={session.data.user.image} />
         <textarea
+          style={{ height: 0 }}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          ref={inputRef}
           name="tweet-form"
           id="tweet-form"
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
