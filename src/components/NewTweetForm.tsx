d6f096be97e82708ca12e7d91909431641da1668
Rangga Aprilio Utama <ranggaaprillio@gmail.com>
2023-08-26 06:17:13 +0700

    fixable unused import

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,6 +1,6 @@
 import { useSession } from "next-auth/react";
 import {
-  FormEvent,
+  type FormEvent,
   useCallback,
   useLayoutEffect,
   useRef,
@@ -44,7 +44,7 @@ function Form() {
       if (session.status !== "authenticated") return;
 
       trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
-        if (oldData == null || oldData.pages[0] == null) return;
+        if (oldData?.pages[0] == null) return;
 
         const newCacheTweet = {
           ...newTweet,
@@ -52,8 +52,8 @@ function Form() {
           likedByMe: false,
           user: {
             id: session.data.user.id,
-            name: session.data.user.name || null,
-            image: session.data.user.image || null,
+            name: session.data.user.name ?? null,
+            image: session.data.user.image ?? null,
           },
         };
 
